e197a2f0fc33c387be92e655a5f8650a893b2554
ayuukumakuma <ayuu.kumakuma@gmail.com>
2025-01-19 17:05:50 +0900

    biomeによるフォーマット

--- vite.config.ts
+++ vite.config.ts
@@ -6,25 +6,25 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ isSsrBuild }) => ({
-  build: {
-    rollupOptions: isSsrBuild
-      ? {
-          input: "./workers/app.ts",
-        }
-      : undefined,
-  },
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
-  plugins: [
-    cloudflareDevProxy({
-      getLoadContext({ context }) {
-        return { cloudflare: context.cloudflare };
-      },
-    }),
-    reactRouter(),
-    tsconfigPaths(),
-  ],
+	build: {
+		rollupOptions: isSsrBuild
+			? {
+					input: "./workers/app.ts",
+				}
+			: undefined,
+	},
+	css: {
+		postcss: {
+			plugins: [tailwindcss, autoprefixer],
+		},
+	},
+	plugins: [
+		cloudflareDevProxy({
+			getLoadContext({ context }) {
+				return { cloudflare: context.cloudflare };
+			},
+		}),
+		reactRouter(),
+		tsconfigPaths(),
+	],
 }));
